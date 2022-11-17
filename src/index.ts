5d29a14dc8efc3ee58e6aa82cda5a1471d79fb24
Timur Bolotov <bolotovtmr@gmail.com>
2022-11-18 02:19:32 +0600

    feat: eslint

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,71 @@
-import * as vscode from 'vscode'
-import path from 'node:path'
-import fs from 'node:fs'
+import * as vscode from "vscode";
+import path from "node:path";
+import fs from "node:fs";
+import { appRoot, reloadWindow } from "./utils";
 
 export async function activate(context: vscode.ExtensionContext) {
-
   // .  /// <reference path="/Users/timur/dev/siebel-web-helper/src/types/siebelapp.d.ts" />
   // FIXME: This is a hack to get the path to the .d.ts file. It should be
   // configurable.
-  const dtsPath = path.join(__dirname, '..', 'src', 'types', 'siebelapp.d.ts')
+  if (!appRoot) {
+    vscode.window
+      .showErrorMessage(
+        "No VS Code root folder to inject types has been found.",
+        "Dismiss",
+        "Reload",
+        "Try Again"
+      )
+      .then((selection) => {
+        if (selection === "Try Again") {
+          activate(context);
+        }
+        if (selection === "Reload") {
+          reloadWindow();
+        }
+        if (selection === "Dismiss") {
+          return;
+        }
+      });
+  }
 
-  const dtsUri = vscode.Uri.file(dtsPath)
+  const dtsPath = path.join(__dirname, "..", "src", "types", "siebelapp.d.ts");
 
-  vscode.Uri.file(dtsPath)
+  const dtsUri = vscode.Uri.file(dtsPath);
 
-  console.log('dtsUri', dtsUri)
-  const siebelAppTypes = vscode.window.showInformationMessage(path.join(__dirname, '..', 'src', 'types', 'siebelapp.d.ts'))
+  vscode.Uri.file(dtsPath);
 
-  const tsRef = `\n/// <reference path="${dtsUri.path}" />`
+  console.log("dtsUri", dtsUri);
+  const siebelAppTypes = vscode.window.showInformationMessage(
+    path.join(__dirname, "..", "src", "types", "siebelapp.d.ts")
+  );
+
+  const tsRef = `\n/// <reference path="${dtsUri.path}" />`;
   // get vscode's installation path
-
 
   // vscode.workspace.fs.writeFile()
 
-  const appRoot = vscode.window.showInformationMessage(vscode.env.appRoot)
+  // const appRoot = vscode.window.showInformationMessage(vscode.env.appRoot)
 
-  const tslibPath = path.join(vscode.env.appRoot, 'extensions', 'node_modules', 'typescript', 'lib', 'lib.es5.d.ts')
+  const tslibPath = path.join(
+    vscode.env.appRoot,
+    "extensions",
+    "node_modules",
+    "typescript",
+    "lib",
+    "lib.es5.d.ts"
+  );
 
-  vscode.workspace.fs.isWritableFileSystem
+  vscode.workspace.fs.isWritableFileSystem;
 
-  fs.appendFile(tslibPath, tsRef, (err) => {
-    if (err) {
-      console.log('Error', err)
-    }
-  })
+  // fs.appendFile(tslibPath, tsRef, (err) => {
+  //   if (err) {
+  //     console.log('Error', err)
+  //   }
+  // })
 
-  console.log(
-    appRoot
-  )
+  console.log(appRoot);
 }
 
-export function deactivate() {}
+export function deactivate() {
+  //
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,30 @@
 import path from "node:path";
-import * as vscode from 'vscode'
+import * as vscode from "vscode";
 
+export const appRoot =
+  vscode.window.showInformationMessage(vscode.env.appRoot) || "";
 
-export const appRoot = await vscode.window.showInformationMessage(vscode.env.appRoot)
+export const tslibPath = path.join(
+  vscode.env.appRoot,
+  "extensions",
+  "node_modules",
+  "typescript",
+  "lib",
+  "lib.es5.d.ts"
+);
 
-export const tslibPath = path.join(vscode.env.appRoot, 'extensions', 'node_modules', 'typescript', 'lib', 'lib.es5.d.ts')
-
-export function getLocalTypesPath(name: string, ext = '.d.ts'): string {
-  return path.join(__dirname, '..', 'src', 'types', name + ext);
+export function getLocalDtsPath(name: string, ext = ".d.ts"): string {
+  return path.join(__dirname, "..", "src", "types", name + ext);
 }
 
 export function toReferencePath(uri: vscode.Uri): string {
   return `\n/// <reference path="${uri.path}" />`;
+}
+
+export function reloadWindow() {
+  return new Promise((resolve) =>
+    vscode.commands
+      .executeCommand("workbench.action.reloadWindow")
+      .then(resolve)
+  );
 }
