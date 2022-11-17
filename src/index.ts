7358c9db60e35bc9a6694d33bdd64b7c69fda47d
Timur Bolotov <bolotovtmr@gmail.com>
2022-11-18 00:16:12 +0600

    feat: add jquery dts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,41 @@
 import * as vscode from 'vscode'
+import path from 'node:path'
+import fs from 'node:fs'
 
-export function activate(context: vscode.ExtensionContext) {
-  vscode.window.showInformationMessage('hello')
+export async function activate(context: vscode.ExtensionContext) {
+
+  // .  /// <reference path="/Users/timur/dev/siebel-web-helper/src/types/siebelapp.d.ts" />
+  // FIXME: This is a hack to get the path to the .d.ts file. It should be
+  // configurable.
+  const dtsPath = path.join(__dirname, '..', 'src', 'types', 'siebelapp.d.ts')
+
+  const dtsUri = vscode.Uri.file(dtsPath)
+
+  vscode.Uri.file(dtsPath)
+
+  console.log('dtsUri', dtsUri)
+  const siebelAppTypes = vscode.window.showInformationMessage(path.join(__dirname, '..', 'src', 'types', 'siebelapp.d.ts'))
+
+  console.log('apptpes', siebelAppTypes)
+  const ref = '\n/// <reference path="' + dtsUri.path + '" />'
+  // get vscode's installation path
+
+
+  // vscode.workspace.fs.writeFile()
+
+  const appRoot = vscode.window.showInformationMessage(vscode.env.appRoot)
+
+  const tslibPath = path.join(vscode.env.appRoot, 'extensions', 'node_modules', 'typescript', 'lib', 'lib.es5.d.ts')
+
+  fs.appendFile(tslibPath, ref, (err) => {
+    if (err) {
+      console.log('Error', err)
+    }
+  })
+
+  console.log(
+    appRoot
+  )
 }
 
 export function deactivate() {}
---
+++ src/types/siebelapp.d.ts
@@ -0,0 +1,15 @@
+/// <reference no-default-lib="true"/>
+
+/*
+Type definitions for Siebel Open UI by Timur Bolotov <https://github.com/timursaurus>
+
+
+November 18, 2022.
+
+*/
+
+declare namespace SiebelApp {
+  namespace S_App {
+    function Get(): string
+  }
+}
